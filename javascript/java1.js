3d409a657cfb59b1a83307e593475af93ab67f78
MrNico073 <andreasars@afk.no>
2025-02-10 14:55:37 +0100

    Update java1.js

--- javascript/java1.js
+++ javascript/java1.js
@@ -1,7 +1,7 @@
 const dictionary = async () => {
     let data = await fetch ('https://random-word-api.herokuapp.com/word?number=9000&length=5')
     let json = await data.json();
-    console.log(json)
+    console.log(json.length)
 }
 
 dictionary()
