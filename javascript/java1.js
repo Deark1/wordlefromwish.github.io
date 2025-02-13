8c572f567702a37d590712b50329f65873888ee8
MrNico073 <andreasars@afk.no>
2025-02-13 15:02:18 +0100

    Update java1.js

--- javascript/java1.js
+++ javascript/java1.js
@@ -19,24 +19,17 @@ let grid = document.getElementById('grid')
 let riktigord = ''
 let antallforsok = 0;
 const maxforsok = 6
+let aktivrute = 0 //holder styr på hvilken rute som er aktiv
 
 
 for (let i = 0;i<30; i++){
     let item = document.createElement('div')
     item.className = 'grid-item'
     item.setAttribute('data-index', i)
+    item.addEventListener('click', () => lagAktivRute(i)) //tryk på rute for å gjøre den aktiv
     grid.appendChild(item)
 
 }
-
-document.addEventListener('keydown', (e) => {
-    let key = e.key.toUpperCase()
-    let ruter = document.querySelectorAll('grid-item')
-
-})
-
-
-
 
 const genererord = async () => {
     let data = await fetch ('https://random-word-api.herokuapp.com/word?length=5')
@@ -48,7 +41,7 @@ const genererord = async () => {
 const startSpill = async () => {
     await genererord()
     antallforsok = 0;
-    document.getElementById('melding').innerText
+    document.getElementById('melding').innerText = 'Skriv inn ditt ord'
     document.getElementById('brukerInnput').value
     document.querySelectorAll('.grid-item').forEach(item => {
         item.innerText=''
@@ -64,7 +57,13 @@ const skjekkOrd = (gjett) =>{
         if (gjett[i] === riktigord[i]){
             resultat.push('riktig')
             ordArray[i]=null
+        } else if (ordArray.includes(gjett[i])) {
+            resultat.push('feilplassert')
+            ordArray[ordArray.indexOf(gjett[i])] = null
+        } else {
+            resultat.push('feil')
         }
+
     }
 }
 const gjettOrd = () => {
